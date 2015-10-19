cbddc3f0ce8109057b2b8a0266a99e4792103ddc
himaaaatti <sux2mfgj@gmail.com>
2015-10-19 19:50:44 +0900

    a

---
+++ arch/x86/kernel/main.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+extern void io_hlt(void);
+
+#define VRAM_TEXT_MODE  0x000B8000
+
+void main(void)
+{
+    uint16_t *vran_text_mode = (uint16_t*)VRAM_TEXT_MODE + 0x10;
+    *vran_text_mode = (0x07 << 8) | 'A';
+
+    *vran_text_mode = (0x07 << 8) | 'B';
+
+    while(true){
+        io_hlt();
+    }
+}
