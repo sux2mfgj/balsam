7a114bd9be9766de25ab3e2c343bf0a3dbada5e5
himaaaatti <sux2mfgj@gmail.com>
2015-10-20 19:00:27 +0900

    fix Makefile

--- arch/x86/boot/loader/loader.asm
+++ arch/x86/boot/loader/loader.asm
@@ -1,8 +1,22 @@
+
+BOOTSEG     equ     0x7c0
+
 [org 0x7c00]
 
-
+    cli
     xor ax, ax,
     mov ds, ax
+    mov es, ax
+    mov ss, ax
+
+    mov sp, 0x7c00
+    sti
+
+
+
+
+
+
 
     mov si, msg
 ch_loop:
@@ -16,7 +30,7 @@ ch_loop:
 hang:
     jmp hang
 
-msg db 'hello world', 13, 10 ,10
+msg db 'hello world', 0
 
 times 510 - ($ - $$) db 0
     db 0x55
