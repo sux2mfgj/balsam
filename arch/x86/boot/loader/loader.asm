14517d396a28d808582310b9b9f9744af9cd7b66
himaaaatti <sux2mfgj@gmail.com>
2015-10-27 08:09:52 +0900

    fix Makefile

--- arch/x86/boot/loader/loader.asm
+++ arch/x86/boot/loader/loader.asm
@@ -1,5 +1,7 @@
 
-BOOTSEG     equ     0x7c0
+BOOTSEG                 equ     0x7c0
+SEGMENT_KERNEL_HEAD     equ     0x7e0
+SEGMENT_KERNEL_SETUP    equ     0x800
 
 [org 0x7c00]
 
@@ -14,24 +16,36 @@ BOOTSEG     equ     0x7c0
 
 load_bootsector:
 
-    mov ax, 0x820
+    mov ax, SEGMENT_KERNEL_HEAD
     mov es, ax
-    mov ch, 0
-    mov dh, 0
-    mov cl, 2
+    mov ch, 0   ; cylinder 0
+    mov dh, 0   ; head 0
+    mov cl, 2   ; sector 2
 
-    mov ah, 0x02
-    mov al, 1
-    mov bx, 0
-    mov dl, 0x00
+    mov ah, 0x02    ; read
+    mov al, 1       ; a sector
+    mov bx, 0       ; es:bx is buffer head addr
+    mov dl, 0x00    ; drive A
     int 0x13
 
     jc error
 
+load_kernel_setup:
+    mov ax, SEGMENT_KERNEL_SETUP
+    mov es, ax
+    mov ch, 0   ; cylinder 0
+    mov dh, 0   ; head 0
+    mov cl, 3   ; sector 3
 
+    mov ah, 0x02    ; read
+    mov al, 1       ; a sector
+    mov bx, 0       ; es:bx is buffer head addr
+    mov dl, 0x00    ; drive A
+    int 0x13
 
+    jc error
 
-    jmp hang
+    jmp SEGMENT_KERNEL_SETUP:0x0
 
 error:
     mov si, msg
